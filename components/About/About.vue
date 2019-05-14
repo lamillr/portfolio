fd695fe56c537e5054e7263ab4d05989f9032e5f
Lisa Miller <lamiller2200@gmail.com>
2019-05-14 10:22:00 -0400

    copy

--- components/About/About.vue
+++ components/About/About.vue
@@ -4,7 +4,7 @@
       About
     </h2>
     <p class="about-p">
-      I'm a self-taught programmer with a good working knowledge of JavaScript ES6, Vue, Python, Sass/CSS3 and HTML5.
+      I'm a self-taught programmer with a good working knowledge of JavaScript ES6+, Vue, Python, Sass/CSS3 and HTML5.
     </p>
     <p class="about-p">
       I love component-driven development, collaboration, learning new technologies, and mastering what I've learned.
